35046ed35eb5a1eb9d860b9b9a3375d7ad50cefb
hamza-dev7 <hamza77dev@gmail.com>
2026-09-08 18:12:37 +0100

    my new nixos config

--- home.nix
+++ home.nix
@@ -5,6 +5,13 @@
   home.homeDirectory = "/home/sicko";
   home.stateVersion = "26.05";
 
+  home.file =  {
+    ".config/noctalia" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dots/noctalia";
+      recursive = true;
+    };
+  };
+
   programs.git = {
     enable = true;
     userName = "hamza-dev7";
